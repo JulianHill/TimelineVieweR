578fd2fbc916c2b44897e80ebb94e8c60e2c4014
JulianHill <jul.hillebrand@gmail.com>
2014-04-22 00:40:16 +0200

    init

---
+++ R/StatisticsVieweR.R
@@ -0,0 +1,152 @@
+Wordcount  <- function(username, no)
+{
+  require(twitteR)
+  require(rCharts)
+  require(tm)
+  
+  #authentication
+  load("auth.Rda")
+  registerTwitterOAuth(twitCred)
+  
+  
+  #get tweets
+  #tweets <- userTimeline(username,n=no)
+  user <- getUser(username)
+  tweets <- userTimeline(username,n=no)
+ 
+  no = length(tweets)
+  df = data.frame(date = 1:no, favorites=1:no, retweets=1:no, stringsAsFactors=FALSE)
+  
+  #fill df
+  tweets <- twListToDF(tweets)
+  
+  tweets.text = tweets$text
+  
+  df$date <- tweets$created
+  df$favorites <- tweets$favoriteCount
+  
+  df$retweets <- tweets$retweetCount
+  
+ 
+  
+  
+  #df = data.frame(followers = 1, following=1, stringsAsFactors=FALSE)
+  #df[1] <- followers
+  #df[2] <- following
+  
+  require(tm)
+  
+  tweets.text <- gsub("amp", "", tweets.text)
+  # build a corpus
+  mydata.corpus <- Corpus(VectorSource(tweets.text))
+  
+  # make each letter lowercase
+  mydata.corpus <- tm_map(mydata.corpus, tolower) 
+  
+  # remove punctuation 
+  mydata.corpus <- tm_map(mydata.corpus, removePunctuation)
+  
+  # remove generic and custom stopwords
+  my_stopwords <- c(stopwords('german'),stopwords('english'))
+  mydata.corpus <- tm_map(mydata.corpus, removeWords, my_stopwords)
+  
+  # build a term-document matrix
+  mydata.dtm <- TermDocumentMatrix(mydata.corpus)
+  
+  # inspect the document-term matrix
+  mydata.dtm
+  
+  # inspect most popular words
+  #findFreqTerms(mydata.dtm)
+  
+  m <- as.matrix(mydata.dtm)
+  v <- sort(rowSums(m),decreasing=TRUE)
+  d <- data.frame(word = names(v),freq=v)
+  
+  wordcount <- dPlot(
+         y="word",
+         x="freq",
+         groups="title",
+         data=d[order(d$freq,decreasing=TRUE)[1:30],],
+         type="bar")
+  
+  wordcount$xAxis(type="addMeasureAxis", outputFormat="#,")
+   wordcount$yAxis(type="addCategoryAxis")
+   #d1
+  
+  wordcount$save("wordcount.html", cdn = TRUE)
+  
+  
+  ##################################################
+  
+  favorites <- df[,c(1,2)]
+  favorites$action <- "favorites"
+  favorites$text <- tweets$text
+  favorites$no <- 1:nrow(favorites)
+  colnames(favorites) <- c("date", "number", "action","text","no")
+  
+  retweets <- df[,c(1,3)]
+  retweets$action <- "retweets"
+  retweets$text <- tweets$text
+  retweets$no <- 1:nrow(retweets)
+  colnames(retweets) <- c("date", "number", "action","text","no")
+  interactions <- rbind(favorites, retweets)
+ 
+  interactions$date <- lapply(interactions$date, as.character)
+  
+      
+  # FINAL!
+  p2 <- nPlot(number ~ no, group = 'action', data = interactions, type = 'scatterChart')
+  p2$xAxis(axisLabel = 'Tweet Number')
+  p2$yAxis(axisLabel = 'Interactions (Favorites + Retweets)')
+  p2$chart(color = c('red', 'blue'))
+  p2$chart(size = '#! function(d){return d.number} !#')
+  p2$chart(tooltipContent = "#! function(key, x, y, e){ 
+         return e.point.date + ': ' + e.point.text   
+} !#")
+ # p2
+  
+ p2$save("interactions.html", cdn = FALSE)
+  
+  ######################################################################
+  #### Linear models:
+  x <-tweets$favoriteCount
+  y <- 1:length(x)
+  lmfit = lm( x ~ y )
+  
+  favorites_fit <- data.frame(date = tweets$created,fitted.values(lmfit),"favorites_fit",favorites$text,favorites$no)
+  colnames(favorites_fit) <- c("date", "number", "action","text","no")
+  
+  x <-tweets$retweetCount
+  y <- 1:length(x)
+  lmfit = lm( x ~ y )
+  
+  retweets_fit <- data.frame(date = tweets$created,fitted.values(lmfit),"retweets_fit",retweets$text,retweets$no)
+  colnames(retweets_fit) <- c("date", "number", "action","text","no")
+  
+  fit <- rbind(favorites_fit, retweets_fit)
+ fit$date <- lapply(fit$date, as.character)
+ 
+ 
+
+##########
+p3<-nPlot(number~no,group="action", data=fit, type="lineChart")
+p3$chart(color = c('brown', 'green'))
+p3$xAxis(axisLabel = 'Tweet Number')
+p3$yAxis(axisLabel = 'Average Interactions (Linear)')
+p3$chart(tooltipContent = "#! function(key, x, y, e){ 
+               return e.point.date + ': ' + e.point.text   
+} !#")
+
+#intchart
+
+
+
+p3$save("fit.html", cdn = FALSE)
+
+
+
+
+
+}
+
---
+++ R/TimelineVieweR.R
@@ -0,0 +1,163 @@
+TwitterTimeline <- function(username,no)
+{
+  require(twitteR)
+  
+  #authentication
+  load("auth.Rda")
+  registerTwitterOAuth(twitCred)
+  
+  
+  #get tweets
+  tweets <- userTimeline(username,n=no)
+  user <- getUser(username)
+  no = length(tweets)
+  df = data.frame(text = 1:no, interactions=1:no, date=1:no,imgs=1:no,ids=1:no, stringsAsFactors=FALSE)
+  
+  
+  tweets <- twListToDF(tweets)
+  
+
+    df$text = tweets$text
+    df$interactions = tweets$retweetCount+tweets$favoriteCount
+    df$date = substr(tweets$created, 1, 19)  
+  df$ids = tweets$id
+
+    
+ #add media
+  
+ 
+ for (i in 1:no)
+ { 
+   tmp = tweets$text[i]
+   
+   url <- sub(".*?http://(.*?) .*", "\\1", tmp)
+   
+   if(grepl(nchar(url),nchar(tmp))){
+     
+     df$imgs[i] <- " / "
+   
+   }else{
+     
+     url <- paste("http://",sub(".*?http://(.*?) .*", "\\1", tmp),sep="")
+     
+     url_long <- decode_short_url(url)
+     
+     df$imgs[i] <- url_long
+   }
+   
+   
+ }
+  
+ 
+  
+  tpl <- "
+  <b>Interactions:</b> {{x.interactions}}<br/>
+  <b>Date:</b> {{x.date}}<br/>
+  "
+  
+  
+  d4 <- apply(df, 1, function(x){
+    list(
+      startDate = gsub("-", ",", as.character(x$date)),
+      headline = x$text,
+      #text = whisker::whisker.render(tpl, list(x = x)),
+      text = paste("URL in Tweet: ",getLongURL.curl(x$imgs),"</br> Number of interactions: ", x$interactions, " </br> Date: ",x$date,sep=""),
+      asset = list(media = "")
+    )
+  })
+  
+  
+  #check if website exists
+  if(length(user$url)>0){ 
+  headl = toString(paste("Twitter Timeline of: ", user$name," </br> <a href ='",user$url,"'>Website</a>",sep=""))
+  }else
+  {
+   headl = toString(paste("Twitter Timeline of: ", user$name,sep="")) 
+    
+  }
+  
+  
+  start <- substr(tweets$created[no], 1, 8)
+  start <- paste(start,"01",sep="")
+  
+  # Create Timeline
+  m = Timeline$new()
+  
+ #START
+ m$main(
+    headline = headl ,
+    type = 'default',
+    text = paste(user$description,"</br> </br> Followers: ",user$followersCount," </br> Following: ",user$friendsCount,sep=""),
+    startDate =  start,
+    #asset = list(media = 'http://r-project.org')
+    asset = list(media = user$profileImageUrl)
+    
+    #asset = list(media = 'https://twitter.com/ArjunaSoriano/status/456342231683592192')
+    
+  )
+  m$config(
+    font = "Merriweather-Newscycle"
+  )
+  names(d4) <- NULL
+  m$event(d4)
+  m$save('index.html')
+  
+  
+  
+  
+  # Modify JS Path to use Local Assets
+  x <- paste(readLines('index.html', warn = F), collapse = '\n')
+  x <- gsub('/Library/Frameworks/R.framework/Versions/3.0/Resources/library/rCharts/libraries/timeline', 'compiled', x)
+  writeLines(x, con = 'index.html')
+  
+  # Browse Page
+  # browseURL('index.html')
+  
+  
+  
+}
+
+decode_short_url <- function(url, ...) {
+  # PACKAGES #
+  require(RCurl)
+  
+  # LOCAL FUNCTIONS #
+  decode <- function(u) {
+    x <- try( getURL(u, header = TRUE, nobody = TRUE, followlocation = FALSE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")) )
+    if(inherits(x, 'try-error') | length(grep(".*Location: (\\S+).*", x))<1) {
+      return(u)
+    } else {
+      return(gsub(".*Location: (\\S+).*", "\\1", x))
+    }
+  }
+  
+  # MAIN #
+  # return decoded URLs
+  urls <- c(url, ...)
+  l <- lapply(urls, decode)
+  names(l) <- urls
+  return(l)
+}
+
+getLongURL.curl <- function(shorturl){
+  # uses curl statement to get expanded url from t.co links (or any link)
+  # loop through until there's no location attribute... that's the long link.
+  newurl <- shorturl
+  url <- ""
+  while(url != newurl){
+    data <- system(paste0("curl -I ", newurl), intern=T)
+    if(sum(grepl("location: ", tolower(data))) == 0){
+      url <- newurl
+    }else{
+      data <- subset(data, tolower(substring(data, 1, 9))=="location:")
+      stringurl <- substring(data[1], 11, nchar(data[1])-1)
+      # sometimes the location data isn't a url.
+      if(substring(stringurl, 1, 4)=="http"){ 
+        newurl <- stringurl
+      }else{
+        url <- newurl
+      }
+    }
+  }
+  return(newurl)
+}
